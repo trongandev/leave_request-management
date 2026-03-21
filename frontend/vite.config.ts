694f7e5949df4f701b918563bb5a67a4a2b015dc
Trong An <troandev@gmail.com>
2026-03-21 10:12:35 +0700

    push

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,17 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-
+import Sitemap from "vite-plugin-sitemap"
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        Sitemap({
+            hostname: "https://lrm.io.vn", // Thay bằng domain của bạn
+            dynamicRoutes: ["/auth/login"], // Các route tĩnh
+        }),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
