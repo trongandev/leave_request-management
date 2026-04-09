d17a367da617516ef16089173cf5bc52f6a72b65
Trong An <troandev@gmail.com>
2026-04-09 16:00:33 +0700

    add new

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -29,12 +29,9 @@ import { DelegationsModule } from './delegations/delegations.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { AuditLogsModule } from './audit-logs/audit-logs.module';
 import { ApprovalStepsFlowLogModule } from './approval-steps-flow-log/approval-steps-flow-log.module';
-<<<<<<< HEAD
 import { PushNotiModule } from './push-noti/push-noti.module';
-=======
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AuditInterceptor } from './common/dto/interceptors/audit.interceptor';
->>>>>>> 116596b863c263a77a66c6fdb1a6e018edc7687d
 
 @Module({
   imports: [
